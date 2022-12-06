06ed0d00a6c737e30aa408e2abc4917ae103f50b
Lennart Pafel <lennart.pafel@code.berlin>
2022-12-06 12:21:40 +0100

    Added documentation

--- app.js
+++ app.js
@@ -4,31 +4,38 @@ const sectBtn = document.querySelectorAll('.control') //Actual Button
 const allSections = document.querySelector('.main-content') // Body
 
 
+//Buttton click active class
 function PageTransitions(){
-    //Buttton click active class
+    //Loops through the toolbar
     for(let i = 0; i < sectBtn.length; i++){
+        //listens for clickevent
         sectBtn[i].addEventListener('click', function() {
+            //sets variable to element with active-btn selector
             let currentBtn = document.querySelectorAll('.active-btn')
+            // replaces classname of variable with empty class
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '')
+            // adds class active-btn to the element the user clicked on 
             this.className += ' active-btn'
         })
     }
 
     //sections active class
     allSections.addEventListener('click', (e) => {
+        //sets variable to id of the clicked element
         const id = e.target.dataset.id;
         if(id){
-            //remove active from the button
+            //remove active from all buttons
             sectBtns.forEach((btn) => {
                 btn.classList.remove('active')
             })
+            //adds class active to the clicked button 
             e.target.classList.add('active')
             
-            //hide other sections
+            //hides sections the user dont want to see
             sections.forEach((section) => {
                 section.classList.remove('active')
             })
-
+            //selects element where user clicked on
             const element = document.getElementById(id);
             element.classList.add('active');
         }
